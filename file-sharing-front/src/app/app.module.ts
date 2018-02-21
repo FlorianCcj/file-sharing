e4687b4476079cf4521f874add3a9544d964c125
florianccj <florianccj@gmail.com>
2018-02-21 21:20:25 +0100

    [+][paginator] add paginator

--- file-sharing-front/src/app/app.module.ts
+++ file-sharing-front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -28,6 +29,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [FilesService],
--- file-sharing-front/src/app/files-list/files-list.component.ts
+++ file-sharing-front/src/app/files-list/files-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import {FilesService} from '../files.service';
 
@@ -11,7 +12,12 @@ import {FilesService} from '../files.service';
 export class FilesListComponent implements OnInit {
 
   filesList: Observable<any>;
+  filesListLength: number;
   mainUrl = this.filesService.mainUrl;
+
+  numberOfDataPerPage = 10;
+  page = 0;
+  maxPage = 0;
   
   constructor(private filesService: FilesService) {}
 
@@ -20,13 +26,46 @@ export class FilesListComponent implements OnInit {
   }
 
   getFiles() {
-    this.filesList = this.filesService.list();
+    this.filesList = this.filesService.list()
+    .map(
+      (filesList: {files: any[]}) => {
+        this.filesListLength = filesList.files.length;
+        this.calculMaxPage()
+        return filesList;
+      }
+    )
   }
 
   deleteFile(file) {
     this.filesService.deleteData(file.id).subscribe(
       () => this.getFiles()
     );
+  }
+
+  switchPage (newPage: number) {
+    if (typeof(newPage) === typeof(+newPage) && +newPage >= 0) {
+      this.page = newPage;
+    }
+  }
+
+  setNumberOfDataPerPage(countData: number) {
+    if (+countData) {
+      if (countData > 0) {
+        this.numberOfDataPerPage = countData;
+      }
+      if (countData > this.filesListLength) {
+        this.numberOfDataPerPage = this.filesListLength;
+      }
+      this.calculMaxPage();
+    }
+
+    this.switchPage(0);
+  }
+
+  calculMaxPage() {
+    const perPage = this.numberOfDataPerPage;
+    const filesLength = this.filesListLength;
+    this.maxPage = filesLength % perPage === 0 ? (filesLength / perPage) - 1 : (filesLength - filesLength % perPage) / perPage; 
   }
 
 }
